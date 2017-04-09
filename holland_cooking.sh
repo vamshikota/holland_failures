13a21ffdcc8e8f96e37849eb7bd60f6868cbcd2f
vamshikota <kvamshikanth@gmail.com>
2017-04-09 18:12:10 +0100

    Update holland_cooking.sh

--- holland_cooking.sh
+++ holland_cooking.sh
@@ -46,7 +46,7 @@ os_short=$(if [[ $os == ubuntu* ]]; then echo ubuntu; else echo rhel; fi;);
 ## Database installed?  if yes what ?
 case "$os_short_version" in
 	rhel6)
-		if [[ -a $(which mysqld) ]]; then 
+		if [[ -a $(which mysqld &> /dev/null) ]]; then 
 			db_name=$(rpm -qf $(which mysqld)); 
 		else
 			db_name=$(rpm -qa| egrep -i 'mysql-server|mysql[0-9][0-9]u?-server|mariadb-server|mariadb[0-9][0-9][0-9]u-server|percona-server-server' | tail -1); 
